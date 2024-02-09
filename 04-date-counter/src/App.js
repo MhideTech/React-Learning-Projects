aed2a9d81fcd25d587804400c610687a0182d208
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-09 21:41:03 +0100

    FInished the "Date Counter" challenge project.... Unto the next🚀

--- 04-date-counter/src/App.js
+++ 04-date-counter/src/App.js
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Hello React</div>
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  const date = new Date("jan 1 2000");
+  date.setDate(date.getDate() + count);
+
+  return (
+    <div>
+      <div className="row">
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <p>Step: {step}</p>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+      <div className="row">
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <p>Count: {count}</p>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <h2>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </h2>
+    </div>
+  );
 }
 
 export default App;
