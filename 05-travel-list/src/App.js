5d5871aa96c9055cd0a7c3bcfbeda65f5c9a14c9
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-10 00:30:05 +0100

    Built the static layout of the travel list project

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,3 +1,36 @@
 export default function App() {
-  return <div>Hello React</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 💼</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        💼 You have X item on your list, and you have already packed X (X%)
+      </em>
+    </footer>
+  );
 }
