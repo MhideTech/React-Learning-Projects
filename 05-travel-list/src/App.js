e4d408bbf4c0595b7be4e36261d44ecc98e0f104
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-12 15:13:08 +0100

    Lifting State Up🆙 : Parent ➡ Child

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -5,11 +5,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(newItem) {
+    setItems((items) => [...items, newItem]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItem={handleAddItems} />
+      <PackingList item={items} />
       <Stats />
     </div>
   );
@@ -19,7 +24,7 @@ function Logo() {
   return <h1>🏝️ Far Away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItem }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -29,7 +34,7 @@ function Form() {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
+    onAddItem(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -56,11 +61,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ item }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {item.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
