51bfeb17cfb70518b2176251098bdc282572f3e4
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-12 20:43:41 +0100

    Displayed appropriate statistics message in the footer section 📉📊

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItems={handleDeleteItems}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -107,11 +107,25 @@ function Item({ item, onDeleteItems, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
+  if (!numItems)
+    return (
+      <footer className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </footer>
+    );
+
   return (
     <footer className="stats">
       <em>
-        💼 You have X item on your list, and you have already packed X (X%)
+        {percentage === 100
+          ? "You got everything! Ready to go ✈"
+          : `💼 You have ${numItems} item on your list, and you have already packed
+        ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   );
