297de3cef452e0608e7fbbfe0d9e9fda1c047146
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-12 12:42:36 +0100

    Finished v2 of date counter

--- 04-date-counter/src/App.js
+++ 04-date-counter/src/App.js
@@ -15,16 +15,36 @@ function Counter() {
   const date = new Date("jan 1 2000");
   date.setDate(date.getDate() + count);
 
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+
   return (
     <div>
-      <div className="row">
+      {/* <div className="row">
         <button onClick={() => setStep((c) => c - 1)}>-</button>
         <p>Step: {step}</p>
         <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div> */}
+      <div className="row">
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(+e.target.value)}
+        />
+        <label>{step}</label>
       </div>
       <div className="row">
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <p>Count: {count}</p>
+        {/* <p>Count: {count}</p> */}
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(+e.target.value)}
+        />
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <h2>
@@ -37,6 +57,11 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </h2>
+      <div className="center">
+        <button onClick={handleReset} className="reset">
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
