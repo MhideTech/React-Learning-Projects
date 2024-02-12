216b867ffa092d62e527b6941abcf118ee0847cf
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-12 21:22:36 +0100

    Implemented sorting functionality 🎭

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -75,10 +75,26 @@ function Form({ onAddItem }) {
 }
 
 function PackingList({ item, onDeleteItems, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = item;
+
+  if (sortBy === "description")
+    sortedItems = item
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = item
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {item.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -87,6 +103,13 @@ function PackingList({ item, onDeleteItems, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
