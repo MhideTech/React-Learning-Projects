119d16fca8d8d39602f27b74c879113c7f22cf2e
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-14 20:57:28 +0100

    Implemented the tip calculator functionality challenge 😆😄

--- 08-tip-calculator/src/App.js
+++ 08-tip-calculator/src/App.js
@@ -1,45 +1,78 @@
+import { useState } from "react";
+
 function App() {
+  const [bill, setBill] = useState("");
+  const [rating, setRating] = useState(0);
+  const [frndRating, setFrndRating] = useState(0);
+
+  const tip = (rating / 100) * bill;
+  const frndTip = (frndRating / 100) * bill;
+  const totalTip = (tip + frndTip) / 2;
+  const totalBill = bill + totalTip;
+
+  function resetValues() {
+    setBill("");
+    setRating(0);
+    setFrndRating(0);
+  }
+
   return (
     <div>
-      <BillInput />
-      <PercentageRating>How did you like the service?</PercentageRating>
-      <PercentageRating>How did your friend like the service?</PercentageRating>
-      <TextOutput />
-      <ResetButton />
+      <BillInput bill={bill} onSetBill={setBill} />
+      <PercentageRating rating={rating} onSetRating={setRating}>
+        How did you like the service?
+      </PercentageRating>
+      <PercentageRating rating={frndRating} onSetRating={setFrndRating}>
+        How did your friend like the service?
+      </PercentageRating>
+      <TextOutput bill={bill} totalTip={totalTip} totalBill={totalBill} />
+      <ResetButton reset={resetValues} />
     </div>
   );
 }
 
-function BillInput() {
+function BillInput({ bill, onSetBill }) {
   return (
     <div className="d-flex">
       <h1>How much was the bill?</h1>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => onSetBill(() => +e.target.value)}
+      />
     </div>
   );
 }
-function PercentageRating({ children }) {
+function PercentageRating({ children, rating, onSetRating }) {
   return (
     <div className="d-flex">
       <h1>{children}</h1>
-      <select>
-        <option>Dissatisfied (0%)</option>
-        <option>It was okay (5%)</option>
-        <option>It was good (10%)</option>
-        <option>Absolutely amazing (20%)</option>
+      <select
+        value={rating}
+        onChange={(e) => onSetRating(() => e.target.value)}
+      >
+        <option value={0}>Dissatisfied (0%)</option>
+        <option value={5}>It was okay (5%)</option>
+        <option value={10}>It was good (10%)</option>
+        <option value={20}>Absolutely amazing (20%)</option>
       </select>
+      x
     </div>
   );
 }
-function TextOutput() {
+
+function TextOutput({ bill, totalTip, totalBill }) {
   return (
     <div className="d-flex">
-      <h1>You pay $105 ($100 + $5 tip)</h1>
+      <h1>
+        You pay ${totalBill} (${bill} + ${totalTip} tip)
+      </h1>
     </div>
   );
 }
-function ResetButton() {
-  return <button>Reset</button>;
+
+function ResetButton({ reset }) {
+  return <button onClick={reset}>Reset</button>;
 }
 
 export default App;
