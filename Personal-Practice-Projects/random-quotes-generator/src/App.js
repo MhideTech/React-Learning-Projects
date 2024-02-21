55cf0aeab27c3666191374a138c94ca8bd129a83
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-21 09:40:05 -0800

    Replaced emoji icons with fontawesome icons

--- Personal-Practice-Projects/random-quotes-generator/src/App.js
+++ Personal-Practice-Projects/random-quotes-generator/src/App.js
@@ -5,24 +5,30 @@ function App() {
         <header>Random Quote Generator</header>
         <div className="content">
           <div className="quote-area">
-            <i>"</i>
+            <i class="fa-solid fa-quote-left"></i>
             <p className="quote">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem
               nobis repellat aut quod iusto vero minus et praesentium ipsam
               nostrum.
             </p>
-            <i>"</i>
+            <i class="fa-solid fa-quote-right"></i>
           </div>
           <div className="author">
-            <span>--</span>
+            <span>~</span>
             <span className="name">Mhizta Orlah</span>
           </div>
           <div className="buttons">
             <div className="features">
               <ul>
-                <li className="sound">🔊</li>
-                <li className="copy">©</li>
-                <li className="twitter">☘</li>
+                <li className="sound">
+                  <i class="fa-solid fa-volume-high"></i>
+                </li>
+                <li className="copy">
+                  <i class="fa-solid fa-copy"></i>
+                </li>
+                <li className="twitter">
+                  <i class="fa-solid fa-share-nodes"></i>
+                </li>
               </ul>
               <button>New Quote</button>
             </div>
