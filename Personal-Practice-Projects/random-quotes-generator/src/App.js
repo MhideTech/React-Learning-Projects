821020a49741ac6214c5cfcec0838fb858f244e4
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-21 09:32:53 -0800

    Built the static part of the application

--- Personal-Practice-Projects/random-quotes-generator/src/App.js
+++ Personal-Practice-Projects/random-quotes-generator/src/App.js
@@ -1,5 +1,36 @@
 function App() {
-  return <div>Hello React!!</div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <header>Random Quote Generator</header>
+        <div className="content">
+          <div className="quote-area">
+            <i>"</i>
+            <p className="quote">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem
+              nobis repellat aut quod iusto vero minus et praesentium ipsam
+              nostrum.
+            </p>
+            <i>"</i>
+          </div>
+          <div className="author">
+            <span>--</span>
+            <span className="name">Mhizta Orlah</span>
+          </div>
+          <div className="buttons">
+            <div className="features">
+              <ul>
+                <li className="sound">🔊</li>
+                <li className="copy">©</li>
+                <li className="twitter">☘</li>
+              </ul>
+              <button>New Quote</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
