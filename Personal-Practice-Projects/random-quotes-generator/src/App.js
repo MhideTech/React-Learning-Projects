397969005c2e35104bce154bdec847e0849f047a
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-21 10:02:36 -0800

    Implemented generating random quotes when button is clicked

--- Personal-Practice-Projects/random-quotes-generator/src/App.js
+++ Personal-Practice-Projects/random-quotes-generator/src/App.js
@@ -1,42 +1,64 @@
+import { useState } from "react";
+
 function App() {
+  const [quote, setQuote] = useState(
+    "No work or love will flourish out of guilt, fear, or hollowness of heart, just as no valid plans for the future can be made by those who have no capacity for living now."
+  );
+  const [author, setAuthor] = useState("Alan Watts");
+
   return (
     <div className="app">
       <div className="container">
         <header>Random Quote Generator</header>
         <div className="content">
           <div className="quote-area">
-            <i class="fa-solid fa-quote-left"></i>
-            <p className="quote">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem
-              nobis repellat aut quod iusto vero minus et praesentium ipsam
-              nostrum.
-            </p>
-            <i class="fa-solid fa-quote-right"></i>
+            <i className="fa-solid fa-quote-left"></i>
+            <p className="quote">{quote}</p>
+            <i className="fa-solid fa-quote-right"></i>
           </div>
           <div className="author">
             <span>~</span>
-            <span className="name">Mhizta Orlah</span>
+            <span className="name">{author}</span>
           </div>
-          <div className="buttons">
-            <div className="features">
-              <ul>
-                <li className="sound">
-                  <i class="fa-solid fa-volume-high"></i>
-                </li>
-                <li className="copy">
-                  <i class="fa-solid fa-copy"></i>
-                </li>
-                <li className="twitter">
-                  <i class="fa-solid fa-share-nodes"></i>
-                </li>
-              </ul>
-              <button>New Quote</button>
-            </div>
-          </div>
+          <Buttons onSetQuote={setQuote} onSetAuthor={setAuthor} />
         </div>
       </div>
     </div>
   );
+}
+
+function Buttons({ onSetQuote, onSetAuthor }) {
+  return (
+    <div className="buttons">
+      <div className="features">
+        <ul>
+          <li className="sound">
+            <i className="fa-solid fa-volume-high"></i>
+          </li>
+          <li className="copy">
+            <i className="fa-solid fa-copy"></i>
+          </li>
+          <li className="twitter">
+            <i className="fa-solid fa-share-nodes"></i>
+          </li>
+        </ul>
+        <NewQuoteButton onSetQuote={onSetQuote} onSetAuthor={onSetAuthor} />
+      </div>
+    </div>
+  );
+}
+
+function NewQuoteButton({ onSetQuote, onSetAuthor }) {
+  function fetchQuotes() {
+    fetch("https://quotable.io/random")
+      .then((res) => res.json())
+      .then((result) => {
+        onSetQuote(result.content);
+        onSetAuthor(result.author);
+      });
+  }
+
+  return <button onClick={fetchQuotes}>New Quotes</button>;
 }
 
 export default App;
