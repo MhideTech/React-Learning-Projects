5e54ca68d32aeaafd9ca81f2eed329b182736074
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-29 11:18:46 -0800

    useEffect to the rescue

--- Intermediate/usepopcorn/src/App.js
+++ Intermediate/usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -98,9 +98,11 @@ const KEY = "3e3243b0";
 export default function App() {
   const [movies, setMovies] = useState([]);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-    .then((res) => res.json())
-    .then((data) => setMovies(data.Search));
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search));
+  }, []);
 
   return (
     <>
