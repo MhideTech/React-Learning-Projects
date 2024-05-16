45b1e51efabcc35bb5d63f8b9899646519df062e
MhideTech <okefolahanolamide2006@gmail.com>
2024-05-16 11:12:35 +0100

    Created Question component to render question when status is active

--- Advanced/01-react-quiz/src/App.js
+++ Advanced/01-react-quiz/src/App.js
@@ -5,10 +5,12 @@ import Main from "./Main";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -17,6 +19,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: "ready" };
     case "dataFailed":
       return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Action unknown");
   }
@@ -41,7 +45,8 @@ export default function App() {
       <Main>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions}  />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
+        {status === 'active' && <Question />}
       </Main>
     </div>
   );
