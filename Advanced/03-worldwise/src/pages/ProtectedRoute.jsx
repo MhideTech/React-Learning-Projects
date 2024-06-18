e6da71431dc8eb377b737b6f0a64d83341554269
MhideTech <okefolahanolamide2006@gmail.com>
2024-06-18 22:15:23 +0100

    .

--- Advanced/03-worldwise/src/pages/ProtectedRoute.jsx
+++ Advanced/03-worldwise/src/pages/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/FakeAuthContext";
 import { useEffect } from "react";
 
+/* eslint-disable react/prop-types */
 function ProtectedRoute({ children }) {
   const { isAuthenticated } = useAuth();
   const navigate = useNavigate();
