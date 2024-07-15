e61c88f8bf52c0b44b4d44120ba25af4bd9c640d
MhideTech <okefolahanolamide2006@gmail.com>
2024-07-15 12:36:20 +0100

    feat: Dispatching Actions from React App

--- Advanced/06-redux-intro/src/features/customers/Customer.js
+++ Advanced/06-redux-intro/src/features/customers/Customer.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 function Customer() {
   const customer = useSelector((store) => store.customer.fullName);
 
-  return <h2>👋 Welcome, {customer}%</h2>;
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
