f54a4825ab9e21904e1fc3d0cc75bfd60ab93995
MhideTech <okefolahanolamide2006@gmail.com>
2024-08-30 10:08:20 +0100

    feat: added menu items to cart

--- Professional/fast-react-pizza/src/features/menu/MenuItem.jsx
+++ Professional/fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,9 +1,23 @@
 /* eslint-disable react/prop-types */
+import { useDispatch } from "react-redux";
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
+import { addItem } from "../cart/cartSlice";
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -17,7 +31,7 @@ function MenuItem({ pizza }) {
         <p className="text-sm capitalize italic text-stone-500">
           {ingredients.join(", ")}
         </p>
-        <div className="mt-auto flex items-center justify-center">
+        <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
             <p className="text-sm">{formatCurrency(unitPrice)}</p>
           ) : (
@@ -25,7 +39,11 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
-          <Button type="small">Add to Cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to Cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
