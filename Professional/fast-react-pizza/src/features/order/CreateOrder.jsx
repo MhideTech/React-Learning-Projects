b9717ce0dacb126c3282dfcf5fc7b7d87e7c7088
MhideTech <okefolahanolamide2006@gmail.com>
2024-08-31 10:15:24 +0100

    feat: used redux thunks to fetch users location using an api with createAsyncThunk function

--- Professional/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ Professional/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -3,10 +3,11 @@ import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
 import EmptyCart from "../cart/EmptyCart";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
 import store from "../../store";
 import { formatCurrency } from "../../utils/helpers";
+import { fetchAddress } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -20,6 +21,7 @@ function CreateOrder() {
   const isSubmitting = navigation.state === "submitting";
 
   const formErrors = useActionData();
+  const dispatch = useDispatch()
 
   const [withPriority, setWithPriority] = useState(false);
   const cart = useSelector(getCart);
@@ -32,6 +34,8 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="text-xl font-semibold mb-8">Ready to order? Lets go!</h2>
+
+      <button onClick={() => dispatch(() => fetchAddress())}>Get position</button>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
