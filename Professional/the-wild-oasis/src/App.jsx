a63ee436694f5dc01d61498f320d946eaad7ef9a
MhideTech <okefolahanolamide2006@gmail.com>
2024-09-12 20:03:25 +0100

    style: used styled component css prop function

--- Professional/the-wild-oasis/src/App.jsx
+++ Professional/the-wild-oasis/src/App.jsx
@@ -2,20 +2,20 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
+import Heading from "./ui/Heading";
 
 function App() {
-  return <>
-  <GlobalStyles />
-    <H1>The wild Oasis</H1>
-    <Button>Check In</Button>
-    <Button>Check Out</Button>
-    <Input type="number" placeholder="Number of guests" />
-  </>;
+  return (
+    <>
+      <GlobalStyles />
+      <Heading as="h1">The wild Oasis</Heading>
+      <Heading as="h2">Check in and out</Heading>
+      <Button>Check In</Button>
+      <Button>Check Out</Button>
+      <Heading as="h3">Form</Heading>
+      <Input type="number" placeholder="Number of guests" />
+    </>
+  );
 }
 
 export default App;
