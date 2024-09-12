34459681d2e185df70478fa9c72e8f14b6267459
MhideTech <okefolahanolamide2006@gmail.com>
2024-09-12 19:04:03 +0100

    style: global styles with styled components

--- Professional/the-wild-oasis/src/App.jsx
+++ Professional/the-wild-oasis/src/App.jsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return <>
+  <GlobalStyles />
+    <H1>The wild Oasis</H1>
+    <Button>Check In</Button>
+    <Button>Check Out</Button>
+    <Input type="number" placeholder="Number of guests" />
+  </>;
 }
 
 export default App;
