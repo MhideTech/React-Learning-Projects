b3e2748422d97325d3aace7c320b289d0d85f7a3
MhideTech <okefolahanolamide2006@gmail.com>
2024-11-10 18:42:24 +0100

    feat: implemented adding an image to supabase

--- Professional/the-wild-oasis/src/services/supabase.js
+++ Professional/the-wild-oasis/src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://yxxlczclizqbpkaxexne.supabase.co";
+export const supabaseUrl = "https://yxxlczclizqbpkaxexne.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4eGxjemNsaXpxYnBrYXhleG5lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyNDA0MDYsImV4cCI6MjA0NDgxNjQwNn0.UomYrw_ak89B3rlc7CNcEsRqBOF9QiBVaSR698UUueY";
 const supabase = createClient(supabaseUrl, supabaseKey);
